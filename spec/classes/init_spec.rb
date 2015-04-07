71b1b5a9c09bb6e47bb3530557a61cccd9cf6cc0
Stefan Möding <stm@kill-9.net>
2015-04-07 20:19:52 +0200

    Add additional simple test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,28 @@
 require 'spec_helper'
-describe 'sendmail' do
 
-  context 'with defaults for all parameters' do
-    it { should contain_class('sendmail') }
+describe 'sendmail', :type => 'class' do
+
+  context "On a Debian OS with no package name specified" do
+    let :facts do
+      {
+        :osfamily => 'Debian'
+      }
+    end
+
+    it {
+      should contain_class('sendmail')
+    }
+  end
+
+  context "On a RedHat OS with no package name specified" do
+    let :facts do
+      {
+        :osfamily => 'RedHat'
+      }
+    end
+
+    it {
+      should contain_class('sendmail')
+    }
   end
 end
