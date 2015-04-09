5c862e0f53155026fc44bc4a035106203f8a7a72
Stefan Möding <stm@kill-9.net>
2015-04-09 07:00:17 +0200

    Remove tests for rspec-puppet-augeas; no fun

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,1 @@
-require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
-require 'rspec-puppet-augeas'
-
-RSpec.configure do |c|
-  c.augeas_fixtures = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures', 'augeas')
-  c.augeas_lensdir = File.join(File.dirname(File.expand_path(__FILE__)), '..', 'lib/augeas/lenses')
-end
