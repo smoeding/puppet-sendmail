01159211c38c579d751a9bdab5cab17246e02eeb
Stefan Möding <stm@kill-9.net>
2015-04-14 19:37:08 +0200

    Introduce sendmail::mc::feature to work on sendmail.mc

---
+++ manifests/mc/feature.pp
@@ -0,0 +1,67 @@
+# = Define: sendmail::mc::feature
+#
+# Add the FEATURE macro to the sendmail.mc file.
+#
+# == Parameters:
+#
+# [*feature_name*]
+#   The name of the feature that will be used. This will be the first
+#   argument of the FEATURE().
+#   **Note**: The feature name should not be quoted as it will always be
+#   quoted in the template.
+#
+# [*args*]
+#   The arguments used for the feature. This must be an array and it will
+#   be used for the following arguments of the FEATURE().
+#   Default value: []
+#
+# [*use_quotes*]
+#   A boolean that indicates if the arguments should be quoted (using
+#   m4 quotes). If this argument is 'true', then the arguments will be
+#   enclosed in ` and ' symbols in the generated output file.
+#   **Note**: The name of the feature will always be quoted.
+#   Valid options: 'true' or 'false'. Default value: 'true'.
+#
+# == Requires:
+#
+# Nothing.
+#
+# == Sample Usage:
+#
+#   sendmail::mc::feature { 'mailertable': }
+#
+#   sendmail::mc::feature { 'mailertable':
+#     args => [ 'hash /etc/mail/mailertable' ],
+#   }
+#
+#   sendmail::mc::feature { 'mailertable':
+#     args       => [ '`hash /etc/mail/mailertable\'' ],
+#     use_quotes => false,
+#   }
+#
+#
+define sendmail::mc::feature (
+  $feature_name = $title,
+  $args         = [],
+  $use_quotes   = true,
+) {
+  include ::sendmail::makeall
+
+  validate_array($args)
+  validate_bool($use_quotes)
+
+  # Add quotes to the args if needed
+  $exp_arg = $use_quotes ? {
+    true  => suffix(prefix($args, '`'), '\''),
+    false => $args,
+  }
+
+  $arr = concat([ "`${feature_name}'" ], $exp_arg)
+
+  concat::fragment { "sendmail_mc-feature-${title}":
+    target  => 'sendmail.mc',
+    order   => '50',
+    content => inline_template("FEATURE(<%= @arr.join(', ') -%>)dnl\n"),
+    notify  => Class['::sendmail::makeall'],
+  }
+}
---
+++ spec/defines/mc/feature_spec.rb
@@ -0,0 +1,102 @@
+require 'spec_helper'
+
+describe 'sendmail::mc::feature' do
+  let(:title) { 'foobar' }
+
+  # :id and :osfamily facts are needed for concat module
+  let(:facts) do
+    {
+      :id              => 'stm',
+      :osfamily        => 'Debian',
+      :operatingsystem => 'Debian',
+      :concat_basedir  => '/tmp',
+    }
+  end
+
+  context 'with no argument' do
+    it do
+      should contain_concat__fragment('sendmail_mc-feature-foobar') \
+              .with_content(/^FEATURE\(`foobar'\)dnl$/) \
+              .with_order('50') \
+              .that_notifies('Class[sendmail::makeall]')
+    end
+  end
+
+  context 'with one argument' do
+    let(:params) do
+      { :args => [ 'foo' ] }
+    end
+
+    it do
+      should contain_concat__fragment('sendmail_mc-feature-foobar') \
+              .with_content(/^FEATURE\(`foobar', `foo'\)dnl$/) \
+              .with_order('50') \
+              .that_notifies('Class[sendmail::makeall]')
+    end
+  end
+
+  context 'with two arguments' do
+    let(:params) do
+      { :args => [ 'foo', 'bar' ] }
+    end
+
+    it do
+      should contain_concat__fragment('sendmail_mc-feature-foobar') \
+              .with_content(/^FEATURE\(`foobar', `foo', `bar'\)dnl$/) \
+              .with_order('50') \
+              .that_notifies('Class[sendmail::makeall]')
+    end
+  end
+
+  context 'with one argument and explicit feature_name' do
+    let(:params) do
+      { :args => [ 'foo' ], :feature_name => 'baz' }
+    end
+
+    it do
+      should contain_concat__fragment('sendmail_mc-feature-foobar') \
+              .with_content(/^FEATURE\(`baz', `foo'\)dnl$/) \
+              .with_order('50') \
+              .that_notifies('Class[sendmail::makeall]')
+    end
+  end
+
+  context 'with one argument and use_quotes => true' do
+    let(:params) do
+      { :args => [ 'foo' ], :use_quotes => true }
+    end
+
+    it do
+      should contain_concat__fragment('sendmail_mc-feature-foobar') \
+              .with_content(/^FEATURE\(`foobar', `foo'\)dnl$/) \
+              .with_order('50') \
+              .that_notifies('Class[sendmail::makeall]')
+    end
+  end
+
+  context 'with one argument and use_quotes => false' do
+    let(:params) do
+      { :args => [ 'foo' ], :use_quotes => false }
+    end
+
+    it do
+      should contain_concat__fragment('sendmail_mc-feature-foobar') \
+              .with_content(/^FEATURE\(`foobar', foo\)dnl$/) \
+              .with_order('50') \
+              .that_notifies('Class[sendmail::makeall]')
+    end
+  end
+
+  context 'with one empty string as argument' do
+    let(:params) do
+      { :args => [ '' ] }
+    end
+
+    it do
+      should contain_concat__fragment('sendmail_mc-feature-foobar') \
+              .with_content(/^FEATURE\(`foobar', `'\)dnl$/) \
+              .with_order('50') \
+              .that_notifies('Class[sendmail::makeall]')
+    end
+  end
+end
