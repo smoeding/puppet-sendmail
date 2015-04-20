88a3f124a595081ed5fbf2a40f442e26fe2b7239
Stefan Möding <stm@kill-9.net>
2015-04-20 20:44:40 +0200

    Added class to manage submit.mc

---
+++ manifests/submit.pp
@@ -0,0 +1,53 @@
+# = Class: sendmail::submit
+#
+# Manage the submit.mc file for the Sendmail MTA.
+#
+# == Parameters:
+#
+# [*msp_host*]
+#   The host where the message submission program should deliver to. This
+#   can be a hostname or IP address. To prevent MX lookups for the host,
+#   put it in square brackets (e.g., [hostname]). Delivery to the local
+#   host would therefore use either [127.0.0.1] for IPv4 or [IPv6:::1]
+#   for IPv6.
+#
+# [*msp_port*]
+#   The port used for the message submission program. Can be a port number
+#   (e.g., 25) or the string MSA for delivery to the message submission
+#   agent on port 587.
+#
+# [*masquerade_as*]
+#   Activate masquerading for the message submission program.
+#
+# == Requires:
+#
+# Nothing.
+#
+# == Sample Usage:
+#
+#   class { 'sendmail::submit':
+#     msp_port      => '25',
+#     masquerade_as => 'example.org',
+#   }
+#
+#
+class sendmail::submit (
+  $msp_host      = '[127.0.0.1]',
+  $msp_port      = 'MSA',
+  $masquerade_as = undef,
+) inherits sendmail::params {
+
+  include ::sendmail::makeall
+  include ::sendmail::service
+
+  validate_re($msp_port, [ '^[0-9]+$', '^MSA$' ])
+
+  file { $::sendmail::params::submit_mc_file:
+    ensure  => file,
+    owner   => 'root',
+    group   => $sendmail::params::sendmail_group,
+    mode    => '0644',
+    content => template('sendmail/submit.mc.erb'),
+    notify  => [ Class['::sendmail::makeall'], Class['::sendmail::service'], ],
+  }
+}
---
+++ spec/classes/submit_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+
+describe 'sendmail::submit' do
+  let(:title) { 'submit' }
+
+  let :facts do
+    { :operatingsystem => 'Debian' }
+  end
+
+  context 'On Debian with default parameters' do
+    it {
+      should contain_file('/etc/mail/submit.mc').with(
+               'ensure' => 'file',
+               'owner'  => 'root',
+               'group'  => 'smmsp',
+               'mode'   => '0644') \
+              .that_notifies('Class[sendmail::makeall]') \
+              .that_notifies('Class[sendmail::service]') \
+              .with_content(/^FEATURE\(`msp', `\[127.0.0.1\]', `MSA'\)dnl$/)
+
+    }
+  end
+
+  context 'On Debian with msp_host => relay.example.org' do
+    let(:params) do
+      { :msp_host => 'relay.example.org' }
+    end
+
+    it {
+      should contain_file('/etc/mail/submit.mc') \
+              .with_content(/^FEATURE\(`msp', `relay.example.org', `MSA'\)dnl$/)
+    }
+  end
+
+  context 'On Debian with msp_port => 25' do
+    let(:params) do
+      { :msp_port => '25' }
+    end
+
+    it {
+      should contain_file('/etc/mail/submit.mc') \
+              .with_content(/^FEATURE\(`msp', `\[127.0.0.1\]', `25'\)dnl$/)
+    }
+  end
+
+  context 'On Debian with masquerade_as => example.org' do
+    let(:params) do
+      { :masquerade_as => 'example.org' }
+    end
+
+    it {
+      should contain_file('/etc/mail/submit.mc') \
+              .with_content(/^MASQUERADE_AS\(`example.org'\)dnl$/) \
+              .with_content(/^FEATURE\(`masquerade_envelope'\)dnl$/)
+
+    }
+  end
+end
