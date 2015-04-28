f976f1b3d813295251f7d586aecfc4fb38a8f80e
Stefan Möding <stm@kill-9.net>
2015-04-28 21:32:36 +0200

    Add more parameters to sendmail::mc

--- manifests/mc.pp
+++ manifests/mc.pp
@@ -4,7 +4,17 @@
 #
 # == Parameters:
 #
-# None.
+# [*sendmail_domain*]
+#
+# [*smart_host*]
+#
+# [*log_level*]
+#
+# [*dont_probe_interfaces*]
+#
+# [*enable_ipv4_daemon*]
+#
+# [*enable_ipv6_daemon*]
 #
 # == Requires:
 #
@@ -16,10 +26,12 @@
 #
 #
 class sendmail::mc (
-  $sendmail_domain    = 'generic',
-  $smart_host         = undef,
-  $enable_ipv4_daemon = true,
-  $enable_ipv6_daemon = true,
+  $sendmail_domain       = 'generic',
+  $smart_host            = undef,
+  $log_level             = undef,
+  $dont_probe_interfaces = undef,
+  $enable_ipv4_daemon    = true,
+  $enable_ipv6_daemon    = true,
 ) inherits ::sendmail::params {
 
   include ::sendmail::makeall
@@ -59,7 +71,9 @@ class sendmail::mc (
     notify  => Class['::sendmail::makeall'],
   }
 
-  ::sendmail::mc::domain { $sendmail_domain: }
+  if ($sendmail_domain != undef) {
+    ::sendmail::mc::domain { $sendmail_domain: }
+  }
 
   if ($smart_host != undef) {
     ::sendmail::mc::define { 'SMART_HOST':
@@ -67,14 +81,25 @@ class sendmail::mc (
     }
   }
 
+  if ($log_level != undef) {
+    ::sendmail::mc::define { 'confLOG_LEVEL':
+      expansion => $log_level,
+    }
+  }
+
+  if ($dont_probe_interfaces != undef) {
+    ::sendmail::mc::define { 'confDONT_PROBE_INTERFACES':
+      expansion => $dont_probe_interfaces,
+    }
+  }
   if ($enable_ipv4_daemon) {
-    ::sendmail::mc::daemon_options { 'IPv4':
+    ::sendmail::mc::daemon_options { 'MTA-v4':
       family => 'inet',
     }
   }
 
   if ($enable_ipv6_daemon) {
-    ::sendmail::mc::daemon_options { 'IPv6':
+    ::sendmail::mc::daemon_options { 'MTA-v6':
       family => 'inet6',
       modify => 'O',
     }
--- spec/classes/mc_spec.rb
+++ spec/classes/mc_spec.rb
@@ -30,20 +30,30 @@ describe 'sendmail::mc' do
       should contain_sendmail__mc__domain('generic')
 
       should_not contain_sendmail__mc__define('SMART_HOST')
+      should_not contain_sendmail__mc__define('confLOG_LEVEL')
+      should_not contain_sendmail__mc__define('confDONT_PROBE_INTERFACES')
 
-      should contain_sendmail__mc__daemon_options('IPv4').with_family('inet')
-      should contain_sendmail__mc__daemon_options('IPv6').with_family('inet6')
-
-      should_not contain_sendmail__mc__define('SMART_HOST')
+      should contain_sendmail__mc__daemon_options('MTA-v4').with_family('inet')
+      should contain_sendmail__mc__daemon_options('MTA-v6').with_family('inet6')
 
       should contain_sendmail__mc__mailer('local')
       should contain_sendmail__mc__mailer('smtp')
     }
   end
 
+  context 'with sendmail_domain => foobar' do
+    let(:params) do
+      { :sendmail_domain => 'foobar' }
+    end
+
+    it {
+      should contain_sendmail__mc__domain('foobar')
+    }
+  end
+
   context 'with smart_host => relay' do
     let(:params) do
-      { 'smart_host' => 'relay' }
+      { :smart_host => 'relay' }
     end
 
     it {
@@ -53,25 +63,67 @@ describe 'sendmail::mc' do
     }
   end
 
-  context 'with enable_ipv4_daemon => false' do
+  context 'with log_level => 7' do
     let(:params) do
-      { 'enable_ipv4_daemon' => false }
+      { :log_level => '7' }
     end
 
     it {
-      should_not contain_sendmail__mc__daemon_options('IPv4')
-      should contain_sendmail__mc__daemon_options('IPv6')
+      should contain_sendmail__mc__define('confLOG_LEVEL').with(
+               'expansion' => '7',
+             )
+    }
+  end
+
+  context 'with dont_probe_interfaces => foo' do
+    let(:params) do
+      { :dont_probe_interfaces => 'foo' }
+    end
+
+    it {
+      should contain_sendmail__mc__define('confDONT_PROBE_INTERFACES').with(
+               'expansion' => 'foo',
+             )
+    }
+  end
+
+  context 'with enable_ipv4_daemon => true' do
+    let(:params) do
+      { :enable_ipv4_daemon => true }
+    end
+
+    it {
+      should contain_sendmail__mc__daemon_options('MTA-v4')
+    }
+  end
+
+  context 'with enable_ipv4_daemon => false' do
+    let(:params) do
+      { :enable_ipv4_daemon => false }
+    end
+
+    it {
+      should_not contain_sendmail__mc__daemon_options('MTA-v4')
+    }
+  end
+
+  context 'with enable_ipv6_daemon => true' do
+    let(:params) do
+      { :enable_ipv6_daemon => true }
+    end
+
+    it {
+      should contain_sendmail__mc__daemon_options('MTA-v6')
     }
   end
 
   context 'with enable_ipv6_daemon => false' do
     let(:params) do
-      { 'enable_ipv6_daemon' => false }
+      { :enable_ipv6_daemon => false }
     end
 
     it {
-      should contain_sendmail__mc__daemon_options('IPv4')
-      should_not contain_sendmail__mc__daemon_options('IPv6')
+      should_not contain_sendmail__mc__daemon_options('MTA-v6')
     }
   end
 end
