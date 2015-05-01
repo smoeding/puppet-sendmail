9675b805ca07252a2222d8a42937dbfc06e64544
Stefan Möding <stm@kill-9.net>
2015-05-01 18:23:02 +0200

    Add parameters for OSTYPE and DOMAIN to submit class

--- manifests/submit.pp
+++ manifests/submit.pp
@@ -4,6 +4,17 @@
 #
 # == Parameters:
 #
+# [*sendmail_ostype*]
+#   The value for the OSTYPE macro in the submit.mc file.
+#   Default value: operating system specific.
+#
+# [*submit_mc_domain*]
+#   The name of the m4 file that holds common data for a domain. This is an
+#   optional configuration item. It may be used by large sites to gather
+#   shared data into one file. Some Linux distributions (e.g. Debian) use
+#   this setting to provide defaults for certain features.
+#   Default value: operating system specific.
+#
 # [*msp_host*]
 #   The host where the message submission program should deliver to. This
 #   can be a hostname or IP address. To prevent MX lookups for the host,
@@ -13,7 +24,7 @@
 #
 # [*msp_port*]
 #   The port used for the message submission program. Can be a port number
-#   (e.g., 25) or the string MSA for delivery to the message submission
+#   (e.g., 25) or the literal 'MSA' for delivery to the message submission
 #   agent on port 587.
 #
 # [*masquerade_as*]
@@ -32,9 +43,11 @@
 #
 #
 class sendmail::submit (
-  $msp_host      = '[127.0.0.1]',
-  $msp_port      = 'MSA',
-  $masquerade_as = undef,
+  $sendmail_ostype  = $::sendmail::params::sendmail_ostype,
+  $submit_mc_domain = $::sendmail::params::submit_mc_domain,
+  $msp_host         = '[127.0.0.1]',
+  $msp_port         = 'MSA',
+  $masquerade_as    = undef,
 ) inherits sendmail::params {
 
   validate_re($msp_port, [ '^[0-9]+$', '^MSA$' ], 'msp_port must be a numeric port number or the literal "MSA"')
