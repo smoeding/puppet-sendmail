4b26c84719caa1cb104d70726b104d0b185d7988
Stefan Möding <stm@kill-9.net>
2015-07-23 18:27:10 +0200

    Update spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,17 @@
+require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |c|
   # :id and :osfamily facts are needed for concat module
   c.default_facts = {
-    :fqdn            => 'mail.example.org',
+    :hostname        => 'cliff',
+    :domain          => 'example.org',
+    :fqdn            => 'cliff.example.org',
     :id              => 'stm',
     :osfamily        => 'Debian',
     :operatingsystem => 'Debian',
     :concat_basedir  => '/var/tmp',
   }
 end
+
+at_exit { RSpec::Puppet::Coverage::report! }
