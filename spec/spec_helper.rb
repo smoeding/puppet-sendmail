07c0c9a2dbd6ab6b008ae6a69430148aed2f1c50
Stefan Möding <stm@kill-9.net>
2015-12-15 17:53:49 +0100

    Add sendmail_version fact to defaults

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,13 +4,14 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 RSpec.configure do |c|
   # :id and :osfamily facts are needed for concat module
   c.default_facts = {
-    :hostname        => 'cliff',
-    :domain          => 'example.org',
-    :fqdn            => 'cliff.example.org',
-    :id              => 'stm',
-    :osfamily        => 'Debian',
-    :operatingsystem => 'Debian',
-    :concat_basedir  => '/var/tmp',
+    :hostname         => 'cliff',
+    :domain           => 'example.org',
+    :fqdn             => 'cliff.example.org',
+    :id               => 'stm',
+    :osfamily         => 'Debian',
+    :operatingsystem  => 'Debian',
+    :concat_basedir   => '/var/tmp',
+    :sendmail_version => '8.14.0',
   }
 end
 
