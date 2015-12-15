a4840e1674c2ade261b5d50d583f6345dad085f7
Stefan Möding <stm@kill-9.net>
2015-12-15 22:07:39 +0100

    Add coverage for sendmail::mc::ostype

--- spec/defines/mc/ostype_spec.rb
+++ spec/defines/mc/ostype_spec.rb
@@ -1,12 +1,25 @@
 require 'spec_helper'
 
 describe 'sendmail::mc::ostype' do
-  let(:title) { 'foonly' }
+  context 'on Debian' do
+    let(:title) { 'debian' }
 
-  it {
-    should contain_concat__fragment('sendmail_mc-ostype-foonly') \
-            .with_content(/^OSTYPE\(`foonly'\)dnl$/) \
-            .with_order('05') \
-            .that_notifies('Class[sendmail::makeall]')
-  }
+    it {
+      should contain_concat__fragment('sendmail_mc-ostype-debian') \
+              .with_content(/^OSTYPE\(`debian'\)dnl$/) \
+              .with_order('05') \
+              .that_notifies('Class[sendmail::makeall]')
+    }
+  end
+
+  context 'on RedHat' do
+    let(:title) { 'redhat' }
+
+    it {
+      should contain_concat__fragment('sendmail_mc-ostype-redhat') \
+              .with_content(/^OSTYPE\(`redhat'\)dnl$/) \
+              .with_order('05') \
+              .that_notifies('Class[sendmail::makeall]')
+    }
+  end
 end
