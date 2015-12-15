8066077328205bb8c24e9450889e522afbac3ac2
Stefan Möding <stm@kill-9.net>
2015-12-15 21:52:27 +0100

    Update test for sendmail::nullclient

--- spec/classes/nullclient_spec.rb
+++ spec/classes/nullclient_spec.rb
@@ -14,12 +14,21 @@ describe 'sendmail::nullclient' do
                'dont_probe_interfaces' => true,
                'enable_ipv4_daemon'    => false,
                'enable_ipv6_daemon'    => false,
-               'enable_access_db'      => false)
+               'enable_access_db'      => false,
+               'mailers'               => []
+             )
 
       should contain_sendmail__mc__feature('no_default_msa')
 
-      should contain_sendmail__mc__feature('nullclient') \
-              .with_args([ 'example.com' ])
+      should contain_sendmail__mc__daemon_options('MTA').with(
+               'family' => 'inet',
+               'addr'   => '127.0.0.1',
+               'port'   => '587'
+             )
+
+      should contain_sendmail__mc__feature('nullclient').with(
+               'args' => [ 'example.com' ]
+             )
     }
   end
 end
