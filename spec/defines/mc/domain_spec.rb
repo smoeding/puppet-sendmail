51ec496f3247bd61b162d828a5c6af78dc66c674
Stefan Möding <stm@kill-9.net>
2015-12-16 08:18:47 +0100

    Update tests for sendmail::mc::domain

--- spec/defines/mc/domain_spec.rb
+++ spec/defines/mc/domain_spec.rb
@@ -1,12 +1,26 @@
 require 'spec_helper'
 
 describe 'sendmail::mc::domain' do
-  let(:title) { 'foo' }
 
-  it {
-    should contain_concat__fragment('sendmail_mc-domain-foo') \
-            .with_content(/^DOMAIN\(`foo'\)dnl$/) \
-            .with_order('07') \
-            .that_notifies('Class[sendmail::makeall]')
-  }
+  context 'with domain foobar' do
+    let(:title) { 'foobar' }
+
+    it {
+      should contain_concat__fragment('sendmail_mc-domain-foobar') \
+              .with_content(/^DOMAIN\(`foobar'\)dnl$/) \
+              .with_order('07') \
+              .that_notifies('Class[sendmail::makeall]')
+    }
+  end
+
+  context 'with domain debian-mta' do
+    let(:title) { 'debian-mta' }
+
+    it {
+      should contain_concat__fragment('sendmail_mc-domain-debian-mta') \
+              .with_content(/^DOMAIN\(`debian-mta'\)dnl$/) \
+              .with_order('07') \
+              .that_notifies('Class[sendmail::makeall]')
+    }
+  end
 end
