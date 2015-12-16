edbd5c5c58aad0c2e96768dc2c05019e6a700ccb
Stefan Möding <stm@kill-9.net>
2015-12-16 07:49:25 +0100

    Require hiera in spec_helper to prevent DEBUG msg

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,12 @@
 require 'rubygems'
+require 'hiera'
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |c|
+
+  # Hiera
+  # c.hiera_config = File.join('spec', 'fixtures', 'hiera', 'hiera.yaml')
+
   # :id and :osfamily facts are needed for concat module
   c.default_facts = {
     :hostname         => 'cliff',
