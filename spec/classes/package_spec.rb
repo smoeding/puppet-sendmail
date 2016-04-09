6022871b87e4cf6b8663d7fb475ba71535af2fe1
Stefan Möding <stm@kill-9.net>
2016-04-09 14:35:45 +0200

    More tests using rspec-puppet-facts

--- spec/classes/package_spec.rb
+++ spec/classes/package_spec.rb
@@ -4,75 +4,63 @@ describe 'sendmail::package' do
 
   it { should contain_class('sendmail::package') }
 
-  context 'On Debian with defaults' do
-    let(:facts) do
-      {
-        :operatingsystem => 'Debian',
-        :osfamily        => 'Debian'
+  on_supported_os.each do |os, facts|
+    context "on #{os} with default parameters" do
+      let(:facts) { facts }
+
+      case facts[:osfamily]
+      when 'Debian'
+        it {
+          should contain_class('sendmail::package').with(
+                   'package_manage' => true
+                 )
+          should contain_package('sendmail')
+        }
+      when 'RedHat'
+        it {
+          should contain_class('sendmail::package').with(
+                   'package_manage' => true
+                 )
+          should contain_package('sendmail')
+          should contain_package('sendmail-cf')
+        }
+      when 'FreeBSD'
+        it {
+          should contain_class('sendmail::package').with(
+                   'package_manage' => false
+                 )
+          should_not contain_package('sendmail')
+        }
+      end
+    end
+
+    context "on #{os} with package_ensure => latest" do
+      let(:params) do
+        { :package_ensure => 'latest' }
+      end
+
+      it {
+        should contain_package('sendmail').with_ensure('latest')
       }
     end
 
-    it {
-      should contain_class('sendmail::package').with('package_manage' => true)
-      should contain_package('sendmail')
-    }
-  end
+    context "on #{os} with package_manage => false" do
+      let(:params) do
+        { :package_manage => false }
+      end
 
-  context 'On ReHat with defaults' do
-    let(:facts) do
-      {
-        :operatingsystem => 'RedHat',
-        :osfamily        => 'RedHat'
+      it { should_not contain_package('sendmail') }
+    end
+
+    context "on #{os} with auxiliary_packages defined" do
+      let(:params) do
+        { :auxiliary_packages => [ 'foo', 'bar' ] }
+      end
+
+      it {
+        should contain_package('foo')
+        should contain_package('bar')
       }
     end
-
-    it {
-      should contain_class('sendmail::package').with('package_manage' => true)
-      should contain_package('sendmail')
-      should contain_package('sendmail-cf')
-    }
-  end
-
-  context 'On FreeBSD with defaults' do
-    let(:facts) do
-      {
-        :operatingsystem => 'FreeBSD',
-        :osfamily        => 'FreeBSD'
-      }
-    end
-
-    it {
-      should contain_class('sendmail::package').with('package_manage' => false)
-      should_not contain_package('sendmail')
-    }
-  end
-
-  context 'with package_ensure => latest' do
-    let(:params) do
-      { :package_ensure => 'latest' }
-    end
-
-    it {
-      should contain_package('sendmail').with_ensure('latest')
-    }
-  end
-
-  context 'with package_manage => false' do
-    let(:params) do
-      { :package_manage => false }
-    end
-
-    it { should_not contain_package('sendmail') }
-  end
-
-  context 'with auxiliary_packages defined' do
-    let(:params) do
-      { :auxiliary_packages => [ 'foo', 'bar' ] }
-    end
-
-    it {
-      should contain_package('foo')
-      should contain_package('bar')
-    }
   end
 end
--- spec/classes/service_spec.rb
+++ spec/classes/service_spec.rb
@@ -4,98 +4,110 @@ describe 'sendmail::service' do
 
   it { should contain_class('sendmail::service') }
 
-  context 'On Debian with defaults' do
-    let(:facts) do
-      { :operatingsystem => 'Debian' }
+  on_supported_os.each do |os, facts|
+    context "on #{os} with default parameters" do
+      let(:facts) { facts }
+
+      case facts[:osfamily]
+      when 'Debian'
+        it {
+          should contain_service('sendmail').with(
+                   'ensure'    => 'running',
+                   'name'      => 'sendmail',
+                   'enable'    => 'true',
+                   'hasstatus' => 'false',
+                 )
+        }
+      else
+        it {
+          should contain_service('sendmail').with(
+                   'ensure'    => 'running',
+                   'name'      => 'sendmail',
+                   'enable'    => 'true',
+                   'hasstatus' => 'true',
+                 )
+        }
+      end
     end
 
-    it {
-      should contain_service('sendmail').with(
-               'ensure'    => 'running',
-               'name'      => 'sendmail',
-               'enable'    => 'true',
-               'hasstatus' => 'false',
-             )
-    }
-  end
+    context "on #{os} with service_manage => false" do
+      let(:params) do
+        { :service_manage => false }
+      end
 
-  context 'with service_manage => false' do
-    let(:params) do
-      { :service_manage => false }
+      it { should_not contain_service('sendmail') }
     end
 
-    it { should_not contain_service('sendmail') }
-  end
+    context "on #{os} with service_ensure => stopped" do
+      let(:params) do
+        { :service_ensure => 'stopped' }
+      end
 
-  context 'with service_ensure => stopped' do
-    let(:params) do
-      { :service_ensure => 'stopped' }
+      it { should contain_service('sendmail').with_ensure('stopped') }
     end
 
-    it { should contain_service('sendmail').with_ensure('stopped') }
-  end
+    context "on #{os} with service_ensure => running" do
+      let(:params) do
+        { :service_ensure => 'running' }
+      end
 
-  context 'with service_ensure => running' do
-    let(:params) do
-      { :service_ensure => 'running' }
+      it { should contain_service('sendmail').with_ensure('running') }
     end
 
-    it { should contain_service('sendmail').with_ensure('running') }
-  end
+    context "on #{os} with service_ensure => true" do
+      let(:params) do
+        { :service_ensure => true }
+      end
 
-  context 'with service_ensure => true' do
-    let(:params) do
-      { :service_ensure => true }
+      it { should contain_service('sendmail').with_ensure('running') }
     end
 
-    it { should contain_service('sendmail').with_ensure('running') }
-  end
+    context "on #{os} with service_ensure => false" do
+      let(:params) do
+        { :service_ensure => false }
+      end
 
-  context 'with service_ensure => false' do
-    let(:params) do
-      { :service_ensure => false }
+      it { should contain_service('sendmail').with_ensure('stopped') }
     end
 
-    it { should contain_service('sendmail').with_ensure('stopped') }
-  end
+    context "on #{os} with service_enable => true" do
+      let(:params) do
+        { :service_enable => true }
+      end
 
-  context 'with service_enable => true' do
-    let(:params) do
-      { :service_enable => true }
+      it { should contain_service('sendmail').with_enable('true') }
     end
 
-    it { should contain_service('sendmail').with_enable('true') }
-  end
+    context "on #{os} with service_enable => false" do
+      let(:params) do
+        { :service_enable => false }
+      end
 
-  context 'with service_enable => false' do
-    let(:params) do
-      { :service_enable => false }
+      it { should contain_service('sendmail').with_enable('false') }
     end
 
-    it { should contain_service('sendmail').with_enable('false') }
-  end
+    context "on #{os} with service_name set" do
+      let(:params) do
+        { :service_name => 'sendmoremail' }
+      end
 
-  context 'with service_name set' do
-    let(:params) do
-      { :service_name => 'sendmoremail' }
+      it { should contain_service('sendmail').with_name('sendmoremail') }
     end
 
-    it { should contain_service('sendmail').with_name('sendmoremail') }
-  end
+    context "on #{os} with service_hasstatus => true" do
+      let(:params) do
+        { :service_hasstatus => 'true' }
+      end
 
-  context 'with service_hasstatus => true' do
-    let(:params) do
-      { :service_hasstatus => 'true' }
+      it { should contain_service('sendmail').with_hasstatus('true') }
     end
 
-    it { should contain_service('sendmail').with_hasstatus('true') }
-  end
+    context "on #{os} with service_hasstatus => false" do
+      let(:params) do
+        { :service_hasstatus => 'false' }
+      end
 
-  context 'with service_hasstatus => false' do
-    let(:params) do
-      { :service_hasstatus => 'false' }
+      it { should contain_service('sendmail').with_hasstatus('false') }
     end
-
-    it { should contain_service('sendmail').with_hasstatus('false') }
   end
 end
